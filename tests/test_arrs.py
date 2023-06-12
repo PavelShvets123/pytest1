9373bdfb3518f245bdb7db145f5c1203fb5f703d
PavelShvets <pasha.shvets99@mail.ru>
2023-06-12 14:08:37 +0300

    добавленны тесты

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,14 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([1], 0, "test") == 1
+    assert arrs.get([1, 2, 3, 4, 5], -1, "test") == "test"
+    assert arrs.get([1, 2, 3, 4, 5], 0, ) == 1
+    assert arrs.get(['cat', 'dog', 'mouse'], 0, "test") == 'cat'
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4, -1) == [5, 6, 7, 8, 9]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], 0, 1) == [1]
